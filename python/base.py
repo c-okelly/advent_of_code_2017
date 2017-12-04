8398e61645ac0be92c02a85ddf7f9fc7aa555a78
c-okelly <conor.okelly@murex.com>
2017-12-04 11:43:19 +0000

    Created base python file

---
+++ python/base.py
@@ -0,0 +1,18 @@
+def main():
+
+    puzzleInput = (open("python/day02.txt", "r").read()
+
+    # Part 1
+    assert(part1(puzzleInput) == 0)
+    
+    # Part 2
+    assert(part2(puzzleInput) == 0)
+
+def part1(puzzleInput):
+
+    return 0
+
+def part2(puzzleInput):
+
+    return 0
+
