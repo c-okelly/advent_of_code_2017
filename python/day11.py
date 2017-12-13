913cad5fb85fe722dbfc518d0f1051a97af16024
c-okelly <okellyconor@gmail.com>
2017-12-13 14:20:46 +0000

    Day 11

--- python/day11.py
+++ python/day11.py
@@ -4,44 +4,74 @@ def main():
 
     # Part 1
     assert(part1("ne,ne,ne") == 3)
-    # assert(part1("ne,ne,ne") == 3)
     print(part1(puzzleInput))
     
     # Part 2
-    # assert(part2("") == 0)
-    # print(part2(puzzleInput))
+    print(part2(puzzleInput))
 
 def part1(puzzleInput):
 
     moves = puzzleInput.split(",")
 
     currPos = [0,0,0]
+    furthest = 0
     for move in moves:
         if move == "n":
+            currPos[0] = currPos[0] + 1
+            currPos[2] = currPos[2] - 1
+        elif move == "ne":
+            currPos[0] = currPos[0] + 1
+            currPos[1] = currPos[1] - 1
+        elif move == "nw":
             currPos[1] = currPos[1] + 1
-        # elif move == "ne":
-        #     currPos[1] = currPos[1] + 1
-        #     currPos[0] = currPos[0] + 1
-        # elif move == "nw":
-        #     currPos[1] = currPos[1] + 1
-        #     currPos[0] = currPos[0] - 1
+            currPos[2] = currPos[2] - 1
 
-        # elif move == "s":
-        #     currPos[1] = currPos[1] - 1
-        # elif move == "se":
-        #     currPos[1] = currPos[1] - 1
-        #     currPos[0] = currPos[0] + 1
-        # elif move == "sw":
-        #     currPos[1] = currPos[1] - 1
-        #     currPos[0] = currPos[0] - 1
+        elif move == "s":
+            currPos[0] = currPos[0] - 1
+            currPos[2] = currPos[2] + 1
+        elif move == "se":
+            currPos[1] = currPos[1] - 1
+            currPos[2] = currPos[2] + 1
+        elif move == "sw":
+            currPos[0] = currPos[0] - 1
+            currPos[1] = currPos[1] + 1
 
-    print(currPos)
+        distance = (abs(currPos[0]) + abs(currPos[1]) + abs(currPos[2])) // 2
 
-    return 3
+    return distance
 
 def part2(puzzleInput):
 
-    return 0
+    moves = puzzleInput.split(",")
+
+    currPos = [0,0,0]
+    furthest = 0
+    for move in moves:
+        if move == "n":
+            currPos[0] = currPos[0] + 1
+            currPos[2] = currPos[2] - 1
+        elif move == "ne":
+            currPos[0] = currPos[0] + 1
+            currPos[1] = currPos[1] - 1
+        elif move == "nw":
+            currPos[1] = currPos[1] + 1
+            currPos[2] = currPos[2] - 1
+
+        elif move == "s":
+            currPos[0] = currPos[0] - 1
+            currPos[2] = currPos[2] + 1
+        elif move == "se":
+            currPos[1] = currPos[1] - 1
+            currPos[2] = currPos[2] + 1
+        elif move == "sw":
+            currPos[0] = currPos[0] - 1
+            currPos[1] = currPos[1] + 1
+
+        distance = (abs(currPos[0]) + abs(currPos[1]) + abs(currPos[2])) // 2
+        if distance > furthest:
+            furthest = distance
+
+    return furthest 
 
 if __name__ == "__main__":
     main()
