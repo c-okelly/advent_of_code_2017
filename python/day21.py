494100ee92f1884a0732d805b6b6a2aea750a803
c-okelly <okellyconor@gmail.com>
2018-02-08 18:41:30 +0000

    Small addition to day 21

--- python/day21.py
+++ python/day21.py
@@ -3,11 +3,11 @@ def main():
     puzzleInput = open("python/day21.txt", "r").read()
 
     # Part 1
-    assert(rotatePattern("123/456/789") == "369/258/147")
-    assert(flipPattern("123/456/789") == "321/654/987")
+    # assert(rotatePattern("123/456/789") == "369/258/147")
+    # assert(flipPattern("123/456/789") == "321/654/987")
 #     assert(part1("""../.# => ##./#../...
-# .#./..#/### => #..#/..../..../#..#""") == 0)
-    # print(part1(puzzleInput))
+# .#./..#/### => #..#/..../..../#..#""") == "#..#/..../..../#..#")
+    print(part1(puzzleInput))
     
     # Part 2
     # assert(part2("") == 0)
@@ -16,7 +16,9 @@ def main():
 def part1(puzzleInput):
 
     # starting parttern
-    pattern = "123/456/789"
+    # pattern = ".#./..#/###"
+    # pattern = "123/456/789"
+    pattern = "1234/5678/90ab/cdef"
 
     rules = puzzleInput.split("\n")
     ruleBook = {}
@@ -25,12 +27,69 @@ def part1(puzzleInput):
         parts = rule.split(" => ")
         ruleBook[parts[0]] = parts[1]
     
+    # Make new pattern
+
+    pattern = pattern.split("/")
+    length = len(pattern[0])
+    print(length)
+
+    if length % 2 == 0:
+        newSquareSize = 2
+        jumpBy = 2
+    elif length % 3 == 0:
+        newSquareSize = 3
+        jumpBy = 3
+
+    print(pattern)
+    subSqaures = []
+    # Find start squares
+    for i in range(0,length,jumpBy):
+        for k in range(0,length,jumpBy):
+            # Build sub squares
+            newSubSquare = []
+            for x in range(0,newSquareSize):
+                for y in range(0,newSquareSize):
+                    newSubSquare.append(pattern[i+x][k+y])
+                    print(i+x, k+y)
+                newSubSquare.append("/")
+            # newSubSquare.pop(-1)
+            subSqaures.append(newSubSquare)
+
+    print(subSqaures)
+
+    # get new patterns
+
+
+    # recombine
+
+    # return getNewPattern(ruleBook, pattern)
+
+def getNewPattern(ruleBook, currentPattern):
     # Get all possiblities of current pattern
+    pVersions = getPatternVersions(currentPattern)
+    print(pVersions)
+    print(ruleBook)
+    
+    newPattern = ""
+    for i in pVersions:
+        match =(ruleBook.get(i,0))
+        if match != 0:
+            newPattern = match
 
-    rotatePattern(pattern)
+    return newPattern 
 
 
-    return 0
+def getPatternVersions(pattern):
+
+    versions = []
+    for i in range(0,4):
+        rotated = rotatePattern(pattern)
+        fliped = flipPattern(rotated)
+        versions.append(rotated)
+        versions.append(fliped)
+        pattern = rotated
+    return versions
+
 
 def rotatePattern(pattern):
 
