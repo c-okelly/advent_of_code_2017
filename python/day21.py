768d94c49cb554be640ee5f5eac6a71d904ecc05
c-okelly <okellyconor@gmail.com>
2018-02-14 09:23:09 +0000

    Day 21 and 24

--- python/day21.py
+++ python/day21.py
@@ -11,14 +11,15 @@ def main():
     
     # Part 2
     # assert(part2("") == 0)
-    # print(part2(puzzleInput))
+    print(part2(puzzleInput))
 
 def part1(puzzleInput):
 
     # starting parttern
-    # pattern = ".#./..#/###"
-    # pattern = "123/456/789"
-    pattern = "1234/5678/90ab/cdef"
+    pattern = ".#./..#/###"
+    # startPattern = "123/456/789"
+    # pattern = "1234/5678/90ab/cdef"
+    # startPattern = "123456/78901a/cdefgh/ijklmn/opqrst/uvwyxz"
 
     rules = puzzleInput.split("\n")
     ruleBook = {}
@@ -27,20 +28,59 @@ def part1(puzzleInput):
         parts = rule.split(" => ")
         ruleBook[parts[0]] = parts[1]
     
-    # Make new pattern
+    for i in range(0,5):
+        pattern = sqaureToNewSwquare(pattern, ruleBook)
+        # print("newPattern", pattern)
+        # print()
+    # count hash
+    count = 0
+    for i in pattern:
+        if i == "#":
+            count += 1
 
-    pattern = pattern.split("/")
+    return count
+
+def part2(puzzleInput):
+
+    # starting parttern
+    pattern = ".#./..#/###"
+    # startPattern = "123/456/789"
+    # pattern = "1234/5678/90ab/cdef"
+    # startPattern = "123456/78901a/cdefgh/ijklmn/opqrst/uvwyxz"
+
+    rules = puzzleInput.split("\n")
+    ruleBook = {}
+
+    for rule in rules:
+        parts = rule.split(" => ")
+        ruleBook[parts[0]] = parts[1]
+    
+    for i in range(0,18):
+        print(i)
+        pattern = sqaureToNewSwquare(pattern, ruleBook)
+        # print("newPattern", pattern)
+        # print()
+    # count hash
+    count = 0
+    for i in pattern:
+        if i == "#":
+            count += 1
+            
+    return count
+
+def sqaureToNewSwquare(startPattern, ruleBook):
+    
+    pattern = startPattern.split("/")
     length = len(pattern[0])
-    print(length)
 
-    if length % 2 == 0:
+    if (length % 2 == 0):
         newSquareSize = 2
         jumpBy = 2
-    elif length % 3 == 0:
+    elif (length % 3 == 0):
         newSquareSize = 3
         jumpBy = 3
 
-    print(pattern)
+    # print(pattern)
     subSqaures = []
     # Find start squares
     for i in range(0,length,jumpBy):
@@ -49,26 +89,49 @@ def part1(puzzleInput):
             newSubSquare = []
             for x in range(0,newSquareSize):
                 for y in range(0,newSquareSize):
+                    # print(i+x,k+y)
                     newSubSquare.append(pattern[i+x][k+y])
-                    print(i+x, k+y)
                 newSubSquare.append("/")
-            # newSubSquare.pop(-1)
             subSqaures.append(newSubSquare)
 
-    print(subSqaures)
+    # print("mixed", subSqaures)
+    # Create sub patterns from each array
+    for i in range(0,len(subSqaures)):
+        subSqaures[i] = "".join(subSqaures[i])[:-1]
+    # print("rejoin", subSqaures)
 
-    # get new patterns
+    # get new patterns 
+    for i in range(0,len(subSqaures)):
+        subSqaures[i] = getNewPattern(ruleBook, subSqaures[i])
+    # print("new returned pattern", subSqaures)
+        
+    # split retunr patterns
+    for i in range(0,len(subSqaures)):
+        subSqaures[i] = subSqaures[i].split("/")
+    
+    # print("cleaned", subSqaures)
 
+    # Recombine back into square
+    length += 1
+    newPattern = ""
+    rowSpread = int(length/newSquareSize)
+    for i in range(0,rowSpread**2,rowSpread):
+        emptyArray = [""]*(newSquareSize + 1)
 
-    # recombine
+        for j in range(i,i+rowSpread):
+            for k in range(0,newSquareSize+1):
+                emptyArray[k] += subSqaures[j][k]
+        for i in emptyArray:
+            newPattern += i + "/"
+    newPattern = newPattern[:-1]
 
-    # return getNewPattern(ruleBook, pattern)
+    return newPattern
 
 def getNewPattern(ruleBook, currentPattern):
     # Get all possiblities of current pattern
     pVersions = getPatternVersions(currentPattern)
-    print(pVersions)
-    print(ruleBook)
+    # print(pVersions)
+    # print(ruleBook)
     
     newPattern = ""
     for i in pVersions:
@@ -115,9 +178,5 @@ def flipPattern(pattern):
     
     return result[:-1]
             
-def part2(puzzleInput):
-
-    return 0
-
 if __name__ == "__main__":
     main()
--- python/day24.py
+++ python/day24.py
@@ -3,20 +3,153 @@ def main():
     puzzleInput = open("python/day24.txt", "r").read()
 
     # Part 1
-    assert(part1("") == 0)
-    print(part1(puzzleInput))
+    # assert(part1("") == 0)
+    # print(part1(puzzleInput))
     
     # Part 2
-    assert(part2("") == 0)
+    # assert(part2("") == 0)
     print(part2(puzzleInput))
 
 def part1(puzzleInput):
 
-    return 0
+    # Parse input
+    bridgeSections = []
+    for i in puzzleInput.split("\n"):
+        bridgeSections.append([int(i.split("/")[0]), int(i.split("/")[1])])
+
+    currentBridges = []
+    finishedBridges = []
+    # Find start sections
+    for i in bridgeSections:
+        if i[0] == 0 or i[1] == 0 :
+            if i[0] != 0:
+                start = i[::-1]
+            else:
+                start = i
+            partsLeft = bridgeSections[:] 
+            partsLeft.remove(i)
+            currentBridges.append({"currentStructure": [start], "partsLeft": partsLeft})
+    
+    # build as many new bridges as possible
+    while (len(currentBridges) > 0):
+        current = currentBridges.pop(0)
+        endPoint = current.get("currentStructure")[-1][1]
+        # print(endPoint)
+
+        sectionFound = False
+        for section in current.get("partsLeft"):
+            if section[0] == endPoint or section[1] == endPoint:
+                sectionFound = True
+                if section[0] == endPoint:
+                    newSection = section[:]
+                else:
+                    newSection = section[:][::-1]
+                # print(section, newSection)
+
+                # make new part
+                newBridge = current.get("currentStructure")[:]
+                newBridge.append(newSection)
+
+                partsLeft = current.get("partsLeft")[:] 
+                partsLeft.remove(section)
+                # print(newBridge)
+                # print(partsLeft)
+                # print()
+                currentBridges.append({"currentStructure": newBridge, "partsLeft": partsLeft})
+        
+        if sectionFound == False:
+            # print("finished")
+            finishedBridges.append(current)
+
+
+    # find longest bridge
+    len(finishedBridges)
+    maxLength = 0
+    for bridge in finishedBridges:
+        bridgeSections = bridge.get("currentStructure")
+        currenTotal = 0
+        for sec in bridgeSections:
+            for part in sec:
+                currenTotal += part
+                
+        if currenTotal > maxLength:
+            maxLength = currenTotal
+
+    return maxLength
 
 def part2(puzzleInput):
 
-    return 0
+    # Parse input
+    bridgeSections = []
+    for i in puzzleInput.split("\n"):
+        bridgeSections.append([int(i.split("/")[0]), int(i.split("/")[1])])
+
+    currentBridges = []
+    finishedBridges = []
+    # Find start sections
+    for i in bridgeSections:
+        if i[0] == 0 or i[1] == 0 :
+            if i[0] != 0:
+                start = i[::-1]
+            else:
+                start = i
+            partsLeft = bridgeSections[:] 
+            partsLeft.remove(i)
+            currentBridges.append({"currentStructure": [start], "partsLeft": partsLeft})
+
+
+    # build as many new bridges as possible
+    while (len(currentBridges) > 0):
+        current = currentBridges.pop(0)
+        endPoint = current.get("currentStructure")[-1][1]
+        # print(endPoint)
+
+        sectionFound = False
+        for section in current.get("partsLeft"):
+            if section[0] == endPoint or section[1] == endPoint:
+                sectionFound = True
+                if section[0] == endPoint:
+                    newSection = section[:]
+                else:
+                    newSection = section[:][::-1]
+                # print(section, newSection)
+
+                # make new part
+                newBridge = current.get("currentStructure")[:]
+                newBridge.append(newSection)
+
+                partsLeft = current.get("partsLeft")[:] 
+                partsLeft.remove(section)
+                currentBridges.append({"currentStructure": newBridge, "partsLeft": partsLeft})
+        
+        if sectionFound == False:
+            finishedBridges.append(current)
+
+
+    longestBridges = []
+    longest = 0
+    for i in finishedBridges:
+        if len(i.get("currentStructure")) >= longest:
+            longestBridges.append(i)
+            longest = len(i.get("currentStructure"))
+    
+    finalLongest = []
+    for i in longestBridges:
+        if len(i.get("currentStructure")) == longest:
+            finalLongest.append(i)
+    
+    maxLength = 0
+    for bridge in finalLongest:
+        bridgeSections = bridge.get("currentStructure")
+        currenTotal = 0
+        for sec in bridgeSections:
+            for part in sec:
+                currenTotal += part
+                
+        if currenTotal > maxLength:
+            maxLength = currenTotal
+        
+    return maxLength
 
 if __name__ == "__main__":
     main()
